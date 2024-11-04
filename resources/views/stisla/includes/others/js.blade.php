584987a401902bb9b3e1d745c7991d955832657e
Nathasya <nathasya.lintang@circlecreative.dev>
2024-11-05 03:39:43 +0700

    update js

--- resources/views/stisla/includes/others/js.blade.php
+++ resources/views/stisla/includes/others/js.blade.php
@@ -64,7 +64,7 @@
 <script>
   var SIDEBAR_MINI = false;
 </script>
-<script src="{{ asset('stisla/assets/js/my-script.min.js?id=2') }}"></script>
+<script src="{{ asset('stisla/assets/js/my-script.min.js') }}?v={{ time() }}"></script>
 
 <!-- Your custom script -->
 @stack('scripts')
