4fa412a9277bfabbe7ebd4cc76334f1fbbd48b3b
Steeven Andrian <steeven@circlecreative.dev>
2025-01-11 08:28:17 +0000

    bug fix css name

--- resources/views/pages/product-detail-new.blade.php
+++ resources/views/pages/product-detail-new.blade.php
@@ -4,12 +4,12 @@
 @section('style')
 <!-- script -->
 <style>
-    .prouduct-categories li ul li {
+    .product-categories li ul li {
         line-height: 1;
         list-style: none;
         margin-left: 15px;
     }
-    .prouduct-categories li ul li a {
+    .product-categories li ul li a {
         font-size: 12px;
     }
     .ms-dong .slick-slide img {
