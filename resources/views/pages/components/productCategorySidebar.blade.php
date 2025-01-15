3c0dd3081d757610a188911b860193cc1c192a57
Steeven Andrian <steeven@circlecreative.dev>
2025-01-15 19:12:18 +0800

    check list categories

--- resources/views/pages/components/productCategorySidebar.blade.php
+++ resources/views/pages/components/productCategorySidebar.blade.php
@@ -1,10 +1,6 @@
 <h2 class="widget-title">Produk</h2>
 <ul class="product-categories">
 @foreach($getCat as $setCat)
-    @php 
-        $excl_ids = [8, 9, 10, 2]; 
-        if (in_array($setCat->id, $excl_ids)) { continue; } 
-    @endphp
     <li><a href="{{url('/category') .'/'. $setCat->url_title}}" style="font-size:15px;"><b><u>{{$setCat->name}}</u></b></a>
         <ul>
         @php ($resProducts = DB::table('products')->where('sidebar_ordering', 'like', $setCat->id."%")->orderBy('sidebar_ordering', 'asc')->whereNull('deleted_at')->get())
