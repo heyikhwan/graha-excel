b76bbd386a29fed14f78e06e1821a611f0860f0e
Steeven Andrian <steeven@circlecreative.dev>
2025-01-15 19:17:38 +0800

    fix category listing

--- resources/views/pages/components/productCategorySidebar.blade.php
+++ resources/views/pages/components/productCategorySidebar.blade.php
@@ -1,9 +1,13 @@
 <h2 class="widget-title">Produk</h2>
 <ul class="product-categories">
 @foreach($getCat as $setCat)
+    @php
+        $excl_ids = [8, 9, 10, 2];
+        if (in_array($setCat->id, $excl_ids)) { continue; }
+    @endphp
     <li><a href="{{url('/category') .'/'. $setCat->url_title}}" style="font-size:15px;"><b><u>{{$setCat->name}}</u></b></a>
         <ul>
-        @php ($resProducts = DB::table('products')->where('sidebar_ordering', 'like', $setCat->id."%")->orderBy('sidebar_ordering', 'asc')->whereNull('deleted_at')->get())
+        @php ($resProducts = DB::table('products')->where('categories.url_title', $setCat->urlTitle)->leftJoin('categories', 'products.categories', '=', 'categories.id')->orderBy('sidebar_ordering', 'asc')->whereNull('deleted_at')->get())
         @foreach($resProducts as $resProduct)
             <li>
                 <a href="{{url('/product') .'/'. $resProduct->url_title}}">
